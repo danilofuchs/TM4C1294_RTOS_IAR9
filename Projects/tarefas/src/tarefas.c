1c8b5f5c2692bfad54426e2ac32bd7ebaecffa78
Danilo Fuchs <danilo_fuchs@hotmail.com>
2021-07-15 21:20:37 -0300

    chore(lab4): Move to dedicated folder

--- Projects/tarefas/src/tarefas.c
+++ Projects/tarefas/src/tarefas.c
@@ -1,42 +1,43 @@
-#include "cmsis_os2.h"        // CMSIS-RTOS
-#include "driverleds.h"       // device drivers
-#include "system_tm4c1294.h"  // CMSIS-Core
+#include "system_tm4c1294.h" // CMSIS-Core
+#include "driverleds.h" // device drivers
+#include "cmsis_os2.h" // CMSIS-RTOS
 
 osThreadId_t thread1_id, thread2_id;
 
-typedef struct {
-  uint8_t led;
-  uint32_t period;
-} thread_arg_t;
+void thread1(void *arg){
+  uint8_t state = 0;
+  
+  while(1){
+    state ^= LED1;
+    LEDWrite(LED1, state);
+    osDelay(100);
+  } // while
+} // thread1
 
-void blink_thread(void *arg_ptr) {
-  thread_arg_t *arg = (thread_arg_t *)arg_ptr;
+void thread2(void *arg){
   uint8_t state = 0;
   uint32_t tick;
-
-  while (1) {
+  
+  while(1){
     tick = osKernelGetTickCount();
+    
+    state ^= LED2;
+    LEDWrite(LED2, state);
+    
+    osDelayUntil(tick + 100);
+  } // while
+} // thread2
 
-    state ^= arg->led;
-    LEDWrite(arg->led, state);
-
-    osDelayUntil(tick + arg->period);
-  }
-}
-
-void main(void) {
+void main(void){
   LEDInit(LED2 | LED1);
 
   osKernelInitialize();
 
-  thread_arg_t thread1_arg = {.led = LED1, .period = 100};
-  thread_arg_t thread2_arg = {.led = LED2, .period = 200};
+  thread1_id = osThreadNew(thread1, NULL, NULL);
+  thread2_id = osThreadNew(thread2, NULL, NULL);
 
-  thread1_id = osThreadNew(blink_thread, (void *)&thread1_arg, NULL);
-  thread2_id = osThreadNew(blink_thread, (void *)&thread2_arg, NULL);
+  if(osKernelGetState() == osKernelReady)
+    osKernelStart();
 
-  if (osKernelGetState() == osKernelReady) osKernelStart();
-
-  while (1)
-    ;
-}  // main
+  while(1);
+} // main
