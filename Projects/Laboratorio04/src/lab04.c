b025cc26af5fb0164ecab6b87b62fca8a7d59447
Danilo Fuchs <danilo_fuchs@hotmail.com>
2021-07-15 21:41:22 -0300

    feat(lab4): Create 4 independent threads

--- Projects/Laboratorio04/src/lab04.c
+++ Projects/Laboratorio04/src/lab04.c
@@ -2,7 +2,7 @@
 #include "driverleds.h"       // device drivers
 #include "system_tm4c1294.h"  // CMSIS-Core
 
-osThreadId_t thread1_id, thread2_id;
+osThreadId_t thread1_id, thread2_id, thread3_id, thread4_id;
 
 typedef struct {
   uint8_t led;
@@ -25,18 +25,22 @@ void blink_thread(void *arg_ptr) {
 }
 
 void main(void) {
-  LEDInit(LED2 | LED1);
+  LEDInit(LED1 | LED2 | LED3 | LED4);
 
   osKernelInitialize();
 
-  thread_arg_t thread1_arg = {.led = LED1, .period = 100};
-  thread_arg_t thread2_arg = {.led = LED2, .period = 200};
+  thread_arg_t thread1_arg = {.led = LED1, .period = 200};
+  thread_arg_t thread2_arg = {.led = LED2, .period = 300};
+  thread_arg_t thread3_arg = {.led = LED3, .period = 500};
+  thread_arg_t thread4_arg = {.led = LED4, .period = 700};
 
   thread1_id = osThreadNew(blink_thread, (void *)&thread1_arg, NULL);
   thread2_id = osThreadNew(blink_thread, (void *)&thread2_arg, NULL);
+  thread3_id = osThreadNew(blink_thread, (void *)&thread3_arg, NULL);
+  thread4_id = osThreadNew(blink_thread, (void *)&thread4_arg, NULL);
 
   if (osKernelGetState() == osKernelReady) osKernelStart();
 
   while (1)
     ;
-}  // main
+}
