38b0ae86fda1cccd10df63b9b1389bb76fd6f5b9
Danilo Fuchs <danilo_fuchs@hotmail.com>
2021-07-15 21:16:58 -0300

    feat(lab4): Customize blink period using args struct

--- Projects/tarefas/src/tarefas.c
+++ Projects/tarefas/src/tarefas.c
@@ -1,38 +1,42 @@
-#include "system_tm4c1294.h" // CMSIS-Core
-#include "driverleds.h"      // device drivers
-#include "cmsis_os2.h"       // CMSIS-RTOS
+#include "cmsis_os2.h"        // CMSIS-RTOS
+#include "driverleds.h"       // device drivers
+#include "system_tm4c1294.h"  // CMSIS-Core
 
 osThreadId_t thread1_id, thread2_id;
 
-void thread(void *arg)
-{
-  uint8_t led = (uint32_t)arg;
+typedef struct {
+  uint8_t led;
+  uint32_t period;
+} thread_arg_t;
+
+void blink_thread(void *arg_ptr) {
+  thread_arg_t *arg = (thread_arg_t *)arg_ptr;
   uint8_t state = 0;
   uint32_t tick;
 
-  while (1)
-  {
+  while (1) {
     tick = osKernelGetTickCount();
 
-    state ^= led;
-    LEDWrite(led, state);
+    state ^= arg->led;
+    LEDWrite(arg->led, state);
 
-    osDelayUntil(tick + 100);
+    osDelayUntil(tick + arg->period);
   }
 }
 
-void main(void)
-{
+void main(void) {
   LEDInit(LED2 | LED1);
 
   osKernelInitialize();
 
-  thread1_id = osThreadNew(thread, (void *)LED1, NULL);
-  thread2_id = osThreadNew(thread, (void *)LED2, NULL);
+  thread_arg_t thread1_arg = {.led = LED1, .period = 100};
+  thread_arg_t thread2_arg = {.led = LED2, .period = 200};
 
-  if (osKernelGetState() == osKernelReady)
-    osKernelStart();
+  thread1_id = osThreadNew(blink_thread, (void *)&thread1_arg, NULL);
+  thread2_id = osThreadNew(blink_thread, (void *)&thread2_arg, NULL);
+
+  if (osKernelGetState() == osKernelReady) osKernelStart();
 
   while (1)
     ;
-} // main
+}  // main
