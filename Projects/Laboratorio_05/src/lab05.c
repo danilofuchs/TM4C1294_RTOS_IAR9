c80f7420d65ad1f7bf67f9f2921b74900dd8f640
Danilo Fuchs <danilo_fuchs@hotmail.com>
2021-07-22 21:26:58 -0300

    fix(lab5): Buffer indexes should be independent in prod vs cons

--- Projects/Laboratorio_05/src/lab05.c
+++ Projects/Laboratorio_05/src/lab05.c
@@ -4,45 +4,37 @@
 #include "system_tm4c1294.h"  // CMSIS-Core
 
 #define BUFFER_SIZE 8
+#define MAX_COUNT 16
 #define DONT_WAIT 0
 
 // Circular buffer to hold generated items
-typedef struct {
-  uint8_t items[BUFFER_SIZE];
-  uint8_t index;
-} circular_buffer_t;
-
-void circularBufferAddItem(circular_buffer_t* buffer, uint8_t item) {
-  buffer->items[buffer->index] = item;
-  buffer->index = (buffer->index + 1) % BUFFER_SIZE;
-}
-uint8_t circularBufferGetItem(circular_buffer_t* buffer) {
-  return buffer->items[buffer->index];
-}
-
-circular_buffer_t buffer;
+uint8_t buffer[BUFFER_SIZE];
 
 // Semaphores to synchronize the ISR with the consumer thread
 osSemaphoreId_t empty_slots_id, full_slots_id;
 
 osThreadId_t consumer_id;
 
-uint8_t count = 0;
+uint8_t count, index = 0;
 void GPIOJ_Handler(void) {
   ButtonIntClear(USW1);
-  count++;
+  count = (count + 1) % MAX_COUNT;
   osSemaphoreAcquire(empty_slots_id, DONT_WAIT);
-  circularBufferAddItem(&buffer, count);
+  buffer[index] = count;
   osSemaphoreRelease(full_slots_id);
+  index = (index + 1) % BUFFER_SIZE;
 }
 
 void consumer(void* arg) {
+  uint8_t index = 0;
   while (1) {
     osSemaphoreAcquire(full_slots_id, osWaitForever);
-    uint8_t item = circularBufferGetItem(&buffer);
+    uint8_t item = buffer[index];
     osSemaphoreRelease(empty_slots_id);
 
     LEDWrite(LED4 | LED3 | LED2 | LED1, item);
+
+    index = (index + 1) % BUFFER_SIZE;
 
     osDelay(500);
   }
@@ -57,9 +49,9 @@ void main(void) {
 
   consumer_id = osThreadNew(consumer, NULL, NULL);
 
-  // BUFFER_SIZE empty slots
+  // starts with BUFFER_SIZE empty slots
   empty_slots_id = osSemaphoreNew(BUFFER_SIZE, BUFFER_SIZE, NULL);
-  // 0 full slots
+  // starts with 0 full slots
   full_slots_id = osSemaphoreNew(BUFFER_SIZE, 0, NULL);
 
   if (osKernelGetState() == osKernelReady) osKernelStart();
