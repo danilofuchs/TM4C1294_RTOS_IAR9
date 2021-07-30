c59d95feba608c41f8bcff5647805a150b334125
Danilo Fuchs <danilo_fuchs@hotmail.com>
2021-07-29 21:42:10 -0300

    feat(lab6): Software PWM

--- Projects/Laboratorio_06/src/lab06.c
+++ Projects/Laboratorio_06/src/lab06.c
@@ -5,6 +5,11 @@
 
 osThreadId_t main_thread_id;
 
+#define LED_COUNT 4
+uint8_t leds[LED_COUNT] = {LED1, LED2, LED3, LED4};
+
+#define PWM_PERIOD 10
+
 typedef struct {
   uint8_t led;
   osMessageQueueId_t queue_id;
@@ -40,53 +45,60 @@ void main(void) {
     ;
 }
 
-void create_pwm_thread(uint8_t index, uint8_t led) {
-  osMessageQueueId_t queue_id =
-      osMessageQueueNew(QUEUE_MESSAGE_COUNT, QUEUE_MESSAGE_SIZE, NULL);
-
-  pwm_thread_args_t args = {.led = led, .queue_id = queue_id};
-
-  osThreadId_t thread_id = osThreadNew(pwm_thread, (void*)&args, NULL);
-
-  pwm_controller_t controller = {
-      .thread_id = thread_id,
-      .queue_id = queue_id,
-  };
-
-  pwm_controllers[index] = controller;
-}
-
 void main_thread(void* arg) {
-  uint8_t led_index = 0;
+  for (int i = 0; i < LED_COUNT; i++) {
+    uint8_t led = leds[i];
 
-  osMessageQueueId_t queue =
-      osMessageQueueNew(QUEUE_MESSAGE_COUNT, QUEUE_MESSAGE_SIZE, NULL);
+    osMessageQueueId_t queue =
+        osMessageQueueNew(QUEUE_MESSAGE_COUNT, QUEUE_MESSAGE_SIZE, NULL);
 
-  pwm_thread_args_t args = {.led = LED1, .queue_id = queue};
-  pwm_controller_t controller = {
-      .thread_id = osThreadNew(pwm_thread, (void*)&args, NULL),
-      .queue_id = queue,
-  };
-
-  create_pwm_thread(0, LED1);
-  // create_pwm_thread(1, LED2);
-  // create_pwm_thread(2, LED3);
-  // create_pwm_thread(3, LED4);
-
-  while (1) {
-    osDelay(500);
+    pwm_thread_args_t args = {.led = led, .queue_id = queue};
+    pwm_controller_t controller = {
+        .thread_id = osThreadNew(pwm_thread, (void*)&args, NULL),
+        .queue_id = queue,
+    };
   }
+
+  // while (1) {
+  // osMessageQueuePut(pwm_controllers[0].queue_id, (void*)1, 0, osWaitForever);
+  // osMessageQueuePut(pwm_controllers[1].queue_id, (void*)1, 0, osWaitForever);
+  // osMessageQueuePut(pwm_controllers[2].queue_id, (void*)1, 0, osWaitForever);
+  // osMessageQueuePut(pwm_controllers[3].queue_id, (void*)1, 0, osWaitForever);
+  // }
+
+  osDelay(osWaitForever);
 }
 
 void pwm_thread(void* arg_ptr) {
-  uint8_t intensity = 0;
+  uint8_t intensity = 10;
+
   pwm_thread_args_t* arg = (pwm_thread_args_t*)arg_ptr;
 
   uint8_t led = arg->led;
-  osMessageQueueId_t queue_id = arg->queue_id;
+  // osMessageQueueId_t queue_id = arg->queue_id;
+
+  uint32_t tick;
+  uint8_t is_led_on = 0;
 
   while (1) {
-    LEDOn(led);
-    osThreadYield();
+    tick = osKernelGetTickCount();
+
+    // osMessageQueueGet(queue_id, &intensity, NULL, osWaitForever);
+    // if (intensity) {
+    //   LEDOn(led);
+    // } else {
+    //   LEDOff(led);
+    // }
+    uint32_t ticks_on = (PWM_PERIOD * intensity) / 100;
+    uint32_t ticks_off = PWM_PERIOD - ticks_on;
+    if (is_led_on) {
+      LEDOff(led);
+      is_led_on = 0;
+      osDelayUntil(tick + ticks_off);
+    } else {
+      LEDOn(led);
+      is_led_on = 1;
+      osDelayUntil(tick + ticks_on);
+    }
   }
 }
